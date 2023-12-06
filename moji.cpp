62974a0f6077bf84030d7a14c0e83940f34ab744
Owner <Owner@MSI>
2023-12-06 12:25:53 +0900

    teisei

--- moji.cpp
+++ moji.cpp
@@ -2,7 +2,7 @@
 #include"print.h"
 #include"loadData.h"
 int main(void) {
-	char moji[64] = { 0 };
+	char moji[72] = { 0 };
 	loadData("data.txt", moji);
 	print(moji);
 	return 0;
--- print.cpp
+++ print.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 #include "print.h"
 void print(char* moji) {
-	for (int i = 0; i <= 63; i++) {
+	for (int i = 0; i <= 71; i++) {
 		if (moji[i] == '1')printf("–");
 		if (moji[i] == '0')printf("@");
 		if (moji[i] == '\n')printf("\n");
